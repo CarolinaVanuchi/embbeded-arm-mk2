253cd381ab62be6442317c4139a14f8af6805516
CarolinaVanuchi <carolinavanuchi21@gmail.com>
2022-06-09 17:19:28 -0300

    ligando motor direito

--- main/robot.h
+++ main/robot.h
@@ -20,12 +20,6 @@ static xQueueHandle gpio_end_motor_esquerdo = NULL; // M2
 static xQueueHandle gpio_end_motor_direito = NULL;  // M3
 static xQueueHandle thetas = NULL;
 
-static void IRAM_ATTR gpio_isr_handler_base(void *arg)
-{
-    uint32_t gpio_sensor = (uint32_t)arg;
-    xQueueSendFromISR(gpio_end_motor_base, &gpio_sensor, NULL);
-}
-
 static void IRAM_ATTR gpio_isr_handler_esquerdo(void *arg)
 {
     uint32_t gpio_sensor = (uint32_t)arg;
@@ -38,7 +32,19 @@ static void IRAM_ATTR gpio_isr_handler_direito(void *arg)
     xQueueSendFromISR(gpio_end_motor_direito, &gpio_sensor, NULL);
 }
 
-void init_motor_esquerdo(void)
+void init_motor_base(void)
+{
+    gpio_reset_pin(CONFIG_GPIO_MOTOR_BASE_ENABLE);
+    gpio_set_direction(CONFIG_GPIO_MOTOR_BASE_ENABLE, GPIO_MODE_OUTPUT);
+
+    gpio_reset_pin(CONFIG_GPIO_MOTOR_BASE_DIRECAO);
+    gpio_set_direction(CONFIG_GPIO_MOTOR_BASE_DIRECAO, GPIO_MODE_OUTPUT);
+
+    gpio_reset_pin(CONFIG_GPIO_MOTOR_BASE);
+    gpio_set_direction(CONFIG_GPIO_MOTOR_BASE, GPIO_MODE_OUTPUT);
+}
+
+void init_end_left(void)
 {
     gpio_reset_pin(CONFIG_GPIO_END_MOTOR_ESQUERDO);
     gpio_set_direction(CONFIG_GPIO_END_MOTOR_ESQUERDO, GPIO_MODE_INPUT);
@@ -56,7 +62,7 @@ void init_motor_esquerdo(void)
     gpio_isr_handler_add(CONFIG_GPIO_END_MOTOR_ESQUERDO, gpio_isr_handler_esquerdo, (void *)CONFIG_GPIO_END_MOTOR_ESQUERDO);
 }
 
-void init_motor_direito(void)
+void init_end_right(void)
 {
     gpio_reset_pin(CONFIG_GPIO_END_MOTOR_DIREITO);
     gpio_set_direction(CONFIG_GPIO_END_MOTOR_DIREITO, GPIO_MODE_INPUT);
@@ -76,44 +82,58 @@ void init_motor_direito(void)
 
 static void init_robot(void)
 {
+    init_motor_base();
     gpio_install_isr_service(ESP_INTR_FLAG_DEFAULT);
     thetas = xQueueCreate(3, sizeof(List_thetas));
     gpio_end_motor_base = xQueueCreate(1, sizeof(uint8_t));
-    init_motor_esquerdo();
-    init_motor_direito();
+    init_end_left();
+    init_end_right();
 }
 
+void move_base() {
+    gpio_set_level(CONFIG_GPIO_MOTOR_BASE, 1);
+    vTaskDelay(10 / portTICK_PERIOD_MS);
+
+    gpio_set_level(CONFIG_GPIO_MOTOR_BASE, 0);
+    vTaskDelay(10 / portTICK_PERIOD_MS);
+}
 static void task_robot(void *arg)
 {
-    uint8_t input_end_base_sensor;
     uint32_t gpio_sensor_base;
-    uint32_t gpio_sensor_esquerdo;
-    uint32_t gpio_sensor_direito;
+    uint32_t gpio_sensor_left;
+    uint32_t gpio_sensor_right;
     List_thetas itens;
+    
+    gpio_set_level(CONFIG_GPIO_MOTOR_BASE_DIRECAO, 0);
+    gpio_set_level(CONFIG_GPIO_MOTOR_BASE_ENABLE, 0);
 
     while (1)
     {
-        if (xQueueReceive(thetas, &itens, 100))
-        {
-            ESP_LOGI("angles", "%lf...", itens.theta_1);
-            ESP_LOGI("angles", "%lf...", itens.theta_2);
-            ESP_LOGI("angles", "%lf...", itens.theta_3);
-        }
+        
+        move_base();
+       
 
-        if (xQueueReceive(gpio_end_motor_base, &gpio_sensor_base, 500))
-        {
-            ESP_LOGI("ISR", "Fim de curso motor 1...");
-        }
+        // if (xQueueReceive(thetas, &itens, 100))
+        // {
+        //     ESP_LOGI("angles", "%lf...", itens.theta_1);
+        //     ESP_LOGI("angles", "%lf...", itens.theta_2);
+        //     ESP_LOGI("angles", "%lf...", itens.theta_3);
+        // }
 
-        if (xQueueReceive(gpio_end_motor_esquerdo, &gpio_sensor_esquerdo, 500))
-        {
-            ESP_LOGI("ISR", "Fim de curso motor 2...");
-        }
+        // if (xQueueReceive(gpio_end_motor_base, &gpio_sensor_base, 500))
+        // {
+        //     ESP_LOGI("ISR", "Fim de curso motor 1...");
+        // }
 
-        if (xQueueReceive(gpio_end_motor_direito, &gpio_sensor_direito, 500))
-        {
-            ESP_LOGI("ISR", "Fim de curso motor 3...");
-        }
+        // if (xQueueReceive(gpio_end_motor_esquerdo, &gpio_sensor_left, 500))
+        // {
+        //     ESP_LOGI("ISR", "Fim de curso motor 2...");
+        // }
+
+        // if (xQueueReceive(gpio_end_motor_direito, &gpio_sensor_right, 500))
+        // {
+        //     ESP_LOGI("ISR", "Fim de curso motor 3...");
+        // }
     }
 }
 
