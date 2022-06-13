e576c7e2133eda998121a8551c9d6fadbc73dddd
CarolinaVanuchi <carolinavanuchi21@gmail.com>
2022-06-13 12:07:43 -0300

    adiconado pwm no motor da base

--- main/motor_base.h
+++ main/motor_base.h
@@ -6,8 +6,20 @@
 #include "driver/gpio.h"
 #include "sdkconfig.h"
 #include "esp_log.h"
+#include "driver/mcpwm.h"
+
+#define SERVO_MIN_PULSEWIDTH_US (1000) // Minimum pulse width in microsecond
+#define SERVO_MAX_PULSEWIDTH_US (2000) // Maximum pulse width in microsecond
+#define SERVO_MAX_DEGREE (90)          // Maximum angle in degree upto which servo can rotate
+
+#define SERVO_PULSE_GPIO (CONFIG_GPIO_MOTOR_BASE) // GPIO connects to the PWM signal line
 
 static xQueueHandle theta_base = NULL;
+
+static inline uint32_t example_convert_servo_angle_to_duty_us(int angle)
+{
+    return (angle + SERVO_MAX_DEGREE) * (SERVO_MAX_PULSEWIDTH_US - SERVO_MIN_PULSEWIDTH_US) / (2 * SERVO_MAX_DEGREE) + SERVO_MIN_PULSEWIDTH_US;
+}
 
 void init_motor_base(void)
 {
@@ -18,7 +30,17 @@ void init_motor_base(void)
     gpio_set_direction(CONFIG_GPIO_MOTOR_BASE_DIRECAO, GPIO_MODE_OUTPUT);
 
     gpio_reset_pin(CONFIG_GPIO_MOTOR_BASE);
-    gpio_set_direction(CONFIG_GPIO_MOTOR_BASE, GPIO_MODE_OUTPUT);
+    // gpio_set_direction(CONFIG_GPIO_MOTOR_BASE, GPIO_MODE_OUTPUT);
+
+    mcpwm_gpio_init(MCPWM_UNIT_0, MCPWM0A, SERVO_PULSE_GPIO); // To drive a RC servo, one MCPWM generator is enough
+
+    mcpwm_config_t pwm_config = {
+        .frequency = 50,
+        .cmpr_a = 0,
+        .counter_mode = MCPWM_UP_COUNTER,
+        .duty_mode = MCPWM_DUTY_MODE_0,
+    };
+    mcpwm_init(MCPWM_UNIT_0, MCPWM_TIMER_0, &pwm_config);
 
     gpio_set_level(CONFIG_GPIO_MOTOR_BASE_DIRECAO, 0);
     gpio_set_level(CONFIG_GPIO_MOTOR_BASE_ENABLE, 0);
@@ -31,6 +53,7 @@ static void task_motor_base(void *arg)
 {
     init_motor_base();
     double theta_base_value;
+    int angle = -SERVO_MAX_DEGREE;
 
     while (1)
     {
@@ -39,11 +62,9 @@ static void task_motor_base(void *arg)
             ESP_LOGI("base angles", "%lf...", theta_base_value);
         }
 
-        gpio_set_level(CONFIG_GPIO_MOTOR_BASE, 1);
-        vTaskDelay(pdMS_TO_TICKS(10));
-
-        gpio_set_level(CONFIG_GPIO_MOTOR_BASE, 0);
-        vTaskDelay(pdMS_TO_TICKS(10));
+        ESP_ERROR_CHECK(mcpwm_set_duty_in_us(MCPWM_UNIT_0, MCPWM_TIMER_0, MCPWM_OPR_A, example_convert_servo_angle_to_duty_us(angle)));
+        vTaskDelay(pdMS_TO_TICKS(100));
+        angle++;
     }
 }
 
